0d741d6b9053cec766d77db740512a15279754b1
rmiri <re_miriuk@hotmail.com>
2019-12-30 10:13:30 +0000

    Folders and stuff

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User
+    attr_accessor :username
+    def initialize(username:)
+        @username = username
+    end
+end
---
+++ run.rb
@@ -0,0 +1,6 @@
+require "pry"
+require_relative "./app/models/user.rb"
+
+binding.pry
+
+puts "Fire types are the best starters"
