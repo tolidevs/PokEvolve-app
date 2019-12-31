bd780cbf281c01faeea3b863317b2df1a61524e1
flatironschool <t.benson@live.co.uk>
2019-12-31 15:36:49 +0000

    Replaced seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,126 @@
-# User.create(username:"Renata",candies:100)
-# User.create(username:"Toli",candies:101)
+# User.create(username:"",candies:)
+
+u1 = User.create(username:"Renata",candies:100)
+u2 = User.create(username:"Toli",candies:101)
+u3 = User.create(username:"Mariola",candies:5)
+u4 = User.create(username:"Keemo",candies:201)
+u5 = User.create(username:"Jo",candies:70)
+u6 = User.create(username:"James",candies:26)
+u7 = User.create(username:"Sara",candies:55)
+u8 = User.create(username:"Tommy",candies:357)
+u9 = User.create(username:"Santiago",candies:123)
+u10 = User.create(username:"Javier",candies:11)
+u11 = User.create(username:"Amir",candies:234)
+u12 = User.create(username:"Mabel",candies:67)
+u13 = User.create(username:"Tim",candies:96)
+u14 = User.create(username:"Hannah",candies:23)
+u15 = User.create(username:"John",candies:11)
+u16 = User.create(username:"Emma",candies:0)
+u17 = User.create(username:"Julia",candies:17)
 
 
-# PokemonFamily.create(evolution_1:"Pichu",evolution_2:"Pikachu",evolution_3:"Raichu", candies_to_evolve: 20)
-# PokemonFamily.create(evolution_1:"Bulbasaur",evolution_2:"Ivysaur",evolution_3:"Venusaur", candies_to_evolve: 30)
-# PokemonFamily.create(evolution_1:"Charmander",evolution_2:"Charmeleon",evolution_3:"Charizard", candies_to_evolve: 25)
-# PokemonFamily.create(evolution_1:"Squirtle",evolution_2:"Wartortle",evolution_3:"Blastoise", candies_to_evolve: 15)
-# PokemonFamily.create(evolution_1:"Lapras",evolution_2:"Gigantamax Lapras", candies_to_evolve: 300)
 
-PokemonFamily.create(evolution_1:"Meowth",evolution_2:"Persian", candies_to_evolve: 10)
+# PokemonFamily.create(evolution_1:"",evolution_2:"", evolution_3:"", candies_to_evolve: )
 
-# Pokemon.create(pokemon_name:"Ivysaur", pokemon_family_id: 1, user_id: 1)
-# Pokemon.create(pokemon_name:"Charmander", pokemon_family_id: 3, user_id: 1)
-# Pokemon.create(pokemon_name:"Lapras", pokemon_family_id: 9, user_id: 2)
-# Pokemon.create(pokemon_name:"Charmeleon", pokemon_family_id: 7, user_id: 2)
-# Pokemon.create(pokemon_name:"Lapras", pokemon_family_id: 9, user_id: 1)
+PokemonFamily.create(evolution_1:"Pichu",evolution_2:"Pikachu",evolution_3:"Raichu", candies_to_evolve: 20)
+PokemonFamily.create(evolution_1:"Bulbasaur",evolution_2:"Ivysaur",evolution_3:"Venusaur", candies_to_evolve: 30)
+PokemonFamily.create(evolution_1:"Charmander",evolution_2:"Charmeleon",evolution_3:"Charizard", candies_to_evolve: 25)
+PokemonFamily.create(evolution_1:"Squirtle",evolution_2:"Wartortle",evolution_3:"Blastoise", candies_to_evolve: 25)
+PokemonFamily.create(evolution_1:"Caterpie",evolution_2:"Metapod", evolution_3:"Butterfree", candies_to_evolve: 10)
+PokemonFamily.create(evolution_1:"Weedle",evolution_2:"Kakuna", evolution_3:"Beedrill", candies_to_evolve: 10)
+PokemonFamily.create(evolution_1:"Pidgey",evolution_2:"Pidgetto", evolution_3:"Pidgeot", candies_to_evolve: 5)
+PokemonFamily.create(evolution_1:"Totodile",evolution_2:"Crocanaw", evolution_3:"Feraligatr", candies_to_evolve: 30)
+PokemonFamily.create(evolution_1:"Togepi",evolution_2:"Togetic", evolution_3:"Togekiss", candies_to_evolve: 30)
+PokemonFamily.create(evolution_1:"Ryhorn",evolution_2:"Rhydon", evolution_3:"Rhyperior", candies_to_evolve: 75)
+PokemonFamily.create(evolution_1:"Happiny",evolution_2:"Chansey", evolution_3:"Blissey", candies_to_evolve: 100)
+PokemonFamily.create(evolution_1:"Magby",evolution_2:"Magmar", evolution_3:"Magmortar", candies_to_evolve: 50)
+PokemonFamily.create(evolution_1:"Mareep",evolution_2:"Flaaffy", evolution_3:"Ampharos", candies_to_evolve: 40)
+PokemonFamily.create(evolution_1:"Hoppip",evolution_2:"Skiploom", evolution_3:"Jumpluff", candies_to_evolve: 15)
+
+PokemonFamily.create(evolution_1:"Magikarp",evolution_2:"Gyarados", candies_to_evolve: 400)
+PokemonFamily.create(evolution_1:"Rattata",evolution_2:"Raticate", candies_to_evolve: 5)
+PokemonFamily.create(evolution_1:"Spearow",evolution_2:"Fearow", candies_to_evolve: 15)
+PokemonFamily.create(evolution_1:"Sandshrew",evolution_2:"Sandslash", candies_to_evolve: 25)
+PokemonFamily.create(evolution_1:"Vulpix",evolution_2:"Ninetails", candies_to_evolve: 25)
+PokemonFamily.create(evolution_1:"Exeggcute",evolution_2:"Exeggcutor", candies_to_evolve: 40)
+PokemonFamily.create(evolution_1:"Cubone",evolution_2:"Marowak", candies_to_evolve: 50)
+PokemonFamily.create(evolution_1:"Smoochum",evolution_2:"Jynx", candies_to_evolve: 20)
+PokemonFamily.create(evolution_1:"Tangela",evolution_2:"Tangrowth", candies_to_evolve: 30)
+PokemonFamily.create(evolution_1:"Koffing",evolution_2:"Wheezing", candies_to_evolve: 15)
+PokemonFamily.create(evolution_1:"Lickitung",evolution_2:"Lickilicky", candies_to_evolve: 40)
+PokemonFamily.create(evolution_1:"Onix",evolution_2:"Steelix", candies_to_evolve: 30)
+PokemonFamily.create(evolution_1:"Krabby",evolution_2:"Kingler", candies_to_evolve: 50)
+PokemonFamily.create(evolution_1:"Munchlax",evolution_2:"Snorlax", candies_to_evolve: 200)
+PokemonFamily.create(evolution_1:"Meowth",evolution_2:"Persian", candies_to_evolve: 40)
+
+PokemonFamily.create(evolution_1:"Farfech'd")
+PokemonFamily.create(evolution_1:"Pinsir")
+PokemonFamily.create(evolution_1:"Kangaskhan")
+PokemonFamily.create(evolution_1:"Tauros")
+PokemonFamily.create(evolution_1:"Lapras")
+PokemonFamily.create(evolution_1:"Ditto")
+PokemonFamily.create(evolution_1:"Aerodactyl")
+PokemonFamily.create(evolution_1:"Articuno")
+PokemonFamily.create(evolution_1:"Zapados")
+PokemonFamily.create(evolution_1:"Moltres")
+PokemonFamily.create(evolution_1:"Mew")
+PokemonFamily.create(evolution_1:"Mewtwo")
+PokemonFamily.create(evolution_1:"Unown")
+
+# Pokemon.create(pokemon_name:"")
+
+# random
+u1.catch_pokemon("Ivysaur")
+u1.catch_pokemon("Pinsir")
+u1.catch_pokemon("Magikarp")
+u1.catch_pokemon("Aerodactyl")
+u1.catch_pokemon("Munchlax")
+u1.catch_pokemon("Meowth")
+u1.catch_pokemon("Sandslash")
+u1.catch_pokemon("Ryhorn")
+u1.catch_pokemon("Rhyperior")
+u1.catch_pokemon("Chansey")
+u1.catch_pokemon("Blissey")
+u1.catch_pokemon("Krabby")
+u1.catch_pokemon("Mareep")
+
+# random
+u2.catch_pokemon("Pichu")
+u2.catch_pokemon("Pikachu")
+u2.catch_pokemon("Raichu")
+u2.catch_pokemon("Tauros")
+u2.catch_pokemon("Onix")
+u2.catch_pokemon("Wheezing")
+u2.catch_pokemon("Weedle")
+u2.catch_pokemon("Crocanaw")
+
+# doesn't have enough candies to evolve any pokemon
+u3.catch_pokemon("Snorlax")
+
+# has enough candies to evolve all of their pokemon
+u4.catch_pokemon("Pidgey")
+u4.catch_pokemon("Pidgey")
+u4.catch_pokemon("Pidgey")
+u4.catch_pokemon("Pidgey")
+u4.catch_pokemon("Pidgey")
+
+# only has enough candies to evolve one if their pokemon
+u6.catch_pokemon("Chansey")
+u6.catch_pokemon("Magikarp")
+u6.catch_pokemon("Ryhorn")
+u6.catch_pokemon("Totodile")
+
+# Has no pokes that can be evolved as all maxed out
+u6.catch_pokemon("Aerodactyl")
+u6.catch_pokemon("Unown")
+u6.catch_pokemon("Venusaur")
+u6.catch_pokemon("Raticate")
+
+# Only one can be evolved
+u7.catch_pokemon("Vulpix")
+u7.catch_pokemon("Unown")
 
 
-# Pokemon.create(pokemon_name:,pokemon_family_id:,user_id:)
 
-#pokemon name = pokemon_family_id.evolution
+
+
